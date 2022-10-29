8899d5743c32067d526d641680361ac99edb3b38
Максим Бельфер <maxbelf1@gmail.com>
2022-10-29 16:12:21 +0300

    feat: added styles

--- src/pages/CharacterPage/CharacterPage.tsx
+++ src/pages/CharacterPage/CharacterPage.tsx
@@ -16,25 +16,35 @@ export const CharacterPage = () => {
   if (isError) return <p>Error</p>
   if (!isSuccess) return <p>Loading</p>
 
+  const name = character?.name ? <p className="charPage__wrapper__title">Name: {character?.name}</p> : null
+
+  const films = character?.films.length ? <ListView items={character?.films} className="films" title="Films" /> : null
+
+  const shows = character?.tvShows.length ? (
+    <ListView items={character?.tvShows} className="shows" title="TV Shows" />
+  ) : null
+
+  const games = character?.videoGames.length ? (
+    <ListView items={character?.videoGames} className="games" title="Games" />
+  ) : null
+
   return (
     <div className="charPage">
       <img src={character?.imageUrl} alt={character?.name} className="charPage__img" />
       <div className="charPage__wrapper">
-        {character?.name && <p className="charPage__wrapper__title">Name: {character?.name}</p>}
-        {character?.films.length ? <ListView items={character?.films} className="films" title="Films" /> : null}
-        {character?.tvShows.length ? <ListView items={character?.tvShows} className="shows" title="TV Shows" /> : null}
-        {character?.videoGames.length ? (
-          <ListView items={character?.videoGames} className="games" title="Games" />
-        ) : null}
+        {name}
+        {films}
+        {shows}
+        {games}
       </div>
     </div>
   )
 }
 
 const ListView: FC<CharacterPageProps> = ({ items, className, title }) => (
-  <div>
+  <div className="charPage__wrapper__text">
     <p>{title}:</p>
-    <ul className={`charPage__wrapper__${className}`}>
+    <ul className={`charPage__wrapper__text__${className}`}>
       {items.map((film, index) => (
         <li key={index}>{film}</li>
       ))}
